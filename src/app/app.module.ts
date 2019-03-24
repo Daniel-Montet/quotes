723856bd4af6e8ba4fce0cf6ff133d4d534311ef
Daniel Montet <dmontetproff@gmail.com>
2019-03-24 19:46:15 +0300

    Add font awesome to app module , add icons to card vote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +24,8 @@ import { MatButtonModule, MatSidenavModule,MatCardModule } from '@angular/materi
     BrowserAnimationsModule,
     MatButtonModule,
     MatSidenavModule,
-    MatCardModule
+    MatCardModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
