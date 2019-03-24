178fd99ae0d654a0303ecf1c78dcffcfe1e76b88
Daniel Montet <dmontetproff@gmail.com>
2019-03-24 14:09:36 +0300

    Add user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
