7952c3992364618f0caa77686f5c76858923589b
Daniel Montet <dmontetproff@gmail.com>
2019-03-24 14:00:38 +0300

    Add new quote model class

---
+++ src/app/quote.ts
@@ -0,0 +1,4 @@
+export class Quote {
+    constructor(public _editorname?:any,public _quote?:string, public _authorname?:string, public _editorid?:number, public _upvote?:number, public _downvote?:number){}
+
+}
