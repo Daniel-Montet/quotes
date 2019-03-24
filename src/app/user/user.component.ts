ae595d5a328744b81edcfd60071c511ebf6782e7
Daniel Montet <dmontetproff@gmail.com>
2019-03-24 14:16:06 +0300

    add function to listen for fired events for both quotes and votes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,8 @@
+import { Quote } from '../quote';
+//import { User } from '../user';
 import { Component, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-user',
@@ -6,6 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  buttonclick:boolean;
+ // user= new User('James');
+  quoteCard=[];
+  card=["red","blue","green"]
+  
+assign(event:Quote){
+  let usersarrLength= this.quoteCard.length
+  event._editorid=usersarrLength + 1;
+  this.quoteCard.push(event)
+}
+upvote(quote:Quote){
+ quote._upvote+=1
+}
+downvote(quote:Quote){
+  quote._downvote+=1
+}
 
   constructor() { }
 
